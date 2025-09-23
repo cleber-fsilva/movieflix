ab97a235dc881b2d9b07c0a9d55bb116ad6ab88a
cleber <silva1cleber@gmail.com>
2025-09-23 14:14:09 -0300

    feat: Implementa segurança com SpringSecurity, JWT token e rota de login para User

--- src/main/java/br/com/movieflix/repository/UserRepository.java
+++ src/main/java/br/com/movieflix/repository/UserRepository.java
@@ -2,6 +2,13 @@ package br.com.movieflix.repository;
 
 import br.com.movieflix.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<UserDetails> findUserByEmail(String email);
+
 }
